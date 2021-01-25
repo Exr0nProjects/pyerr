2abb88ab72f989a8f9f8c6f52a508a7b76bedfc9
Exr0n <mail@exr0n.com>
2021-01-25 11:31:01 -0800

    data model

--- calculate.py
+++ calculate.py
@@ -5,7 +5,14 @@ import numpy as np
 from ErrorProp import ErroredValue
 
 def get():
-    return (pd.dataframe(
+    return {    # peter tissue orange
+        'name': ('tissues', 'orange'),
+        'data': pd.dataframe(data={
+            'inches': [ 0, 6, 12, 24],
+            'seconds': [400]*4,
+            'counts': [4358, 4116, 4041, 4052]}),
+        'background': { 'seconds': 300, 'counts': 39 }
+    };
 
 if __name__ == '__main__':
     pass
