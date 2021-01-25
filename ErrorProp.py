725c551ab6b05c4a826e31a808d3433a2f0e3736
Houjun Liu <hliu@shabang.cf>
2021-01-25 11:05:25 -0800

    done

--- ErrorProp.py
+++ ErrorProp.py
@@ -4,18 +4,34 @@ class ErroredValue(object):
         self.delta = delta
 
     def __add__(self, o): 
-        return ErroredValue((self.value+o.value), (((self.delta**2) + (o.delta**2))**0.5))
+        if type(o) == ErroredValue:
+            return ErroredValue((self.value+o.value), (((self.delta**2) + (o.delta**2))**0.5))
+        else:
+            return ErroredValue((self.value+o), (((self.delta**2) + 0)**0.5))
 
     def __sub__(self, o):
-        return ErroredValue((self.value-o.value), (((self.delta**2) + (o.delta**2))**0.5))
+        if type(o) == ErroredValue:
+            return ErroredValue((self.value-o.value), (((self.delta**2) + (o.delta**2))**0.5))
+        else:
+            return ErroredValue((self.value-o), (((self.delta**2) + 0)**0.5))
 
     def __mul__(self, o):
-        return ErroredValue((self.value*o.value), (self.value*o.value)*(((self.delta/self.value)**2 + (o.delta/o.value)**2)**0.5))
+        if type(o) == ErroredValue:
+            return ErroredValue((self.value*o.value), (self.value*o.value)*(((self.delta/self.value)**2 + (o.delta/o.value)**2)**0.5))
+        else:
+            return ErroredValue((self.value*o), (self.value*o)*(((self.delta/self.value)**2 + 0)**0.5))
 
     def __div__(self, o):
-        return ErroredValue((self.value/o.value), (self.value/o.value)*(((self.delta/self.value)**2 + (o.delta/o.value)**2)**0.5))
+        if type(o) == ErroredValue:
+            return ErroredValue((self.value/o.value), (self.value/o.value)*(((self.delta/self.value)**2 + (o.delta/o.value)**2)**0.5))
+        else:
+            return ErroredValue((self.value/o), (self.value/o)*(((self.delta/self.value)**2)**0.5))
 
+    def __str__(self):
+        return f'ErroredValue {self.value}±{self.delta:.2f}'
 
+    def __repr__(self):
+        return f'<ErroredValue {self.value}±{self.delta:.2f} at {hex(id(self))}>'
 
 
 
