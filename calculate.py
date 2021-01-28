d08477b742a5706b079ed44b089606b2c4810b4c
Houjun Liu <hliu@shabang.cf>
2021-01-27 21:13:01 -0800

    woo hoo!

--- calculate.py
+++ calculate.py
@@ -1,5 +1,6 @@
 # type:ignore
 
+import tqdm
 import math
 import pandas as pd
 import numpy as np
@@ -41,7 +42,7 @@ def SSE(indicies, prediction, logits, logits_error, function):
     ri = function(prediction, indicies)
     return ((logits-ri)**2/logits_error**2).sum()
 
-def sMinFit(datatable, function, param=1, lr=1e-6, epsilon=1e-8, epochs=2000):
+def sMinFit(datatable, function, param=1, lr=5e-10, epsilon=1e-8, epochs=5000):
     logits = datatable.normalized_count_rate.apply(lambda x:x.value)
     logits_err = datatable.normalized_count_rate.apply(lambda x:x.delta)
     inches  = datatable.inches
@@ -50,15 +51,24 @@ def sMinFit(datatable, function, param=1, lr=1e-6, epsilon=1e-8, epochs=2000):
     ins = []
     outs = []
 
-    for _ in range(epochs):
+    dydx = epsilon
+    bar = tqdm.tqdm(range(epochs))
+
+    for _ in bar:
         # TODO broken
-        dydx = ((SSE(inches, param+epsilon, logits, logits_err, function)-SSE(inches, param, logits, logits_err, function))/epsilon)
-        param = param-dydx*lr
+        param_next = param-(dydx*lr+epsilon)
+        # if param_next<0:
+            # param_next = 1
+        dydx = ((SSE(inches, param, logits, logits_err, function)-SSE(inches, param_next, logits, logits_err, function))/(param-param_next))
+        param = param_next
 
         ins.append(param)
         outs.append(SSE(inches, param+epsilon, logits, logits_err, function))
         # print(param)
         # weight update
 
+        bar.set_description(f'Current fit: {param:2f}, Update: {dydx:2f}')
+
+    print(param)
     return ins, outs
 
